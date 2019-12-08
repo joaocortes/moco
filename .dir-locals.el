fefda6186774bec826d98631715efad54c18287c
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-08 00:19:19 +0000

    better run command

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\"; java -jar ./target/org.sat4j.moco-0.0.1-SNAPSHOT-jar-with-dependencies.jar example2.opb")))
+  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\"")))
