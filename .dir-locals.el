f032f6c919321658e4721e75695fef05fe8c752a
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-11 14:41:33 +0000

    Added a compilation projectile command

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,9 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
+;;;
 
 ((nil
-  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\"; java -jar ./target/org.sat4j.moco-0.0.1-SNAPSHOT-jar-with-dependencies.jar example2.opb")))
+  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\" "
+			      ;;;java -jar ./target/org.sat4j.moco-0.0.1-SNAPSHOT-jar-with-dependencies.jar example2.opb
+			      )
+  (projectile-project-compilation-cmd . "mvn -DskipTests=true package")))
