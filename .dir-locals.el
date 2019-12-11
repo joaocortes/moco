a20ec9f3c142ac11db145116e5fd77e9b164f524
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-11 14:50:17 +0000

    Added alternative run command

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,6 +3,7 @@
 
 ((nil
   (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\"")
+			      ;;;java -jar ./target/org.sat4j.moco-0.0.1-SNAPSHOT-jar-with-dependencies.jar example2.opb
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (lsp-file-watch-threshold)))
