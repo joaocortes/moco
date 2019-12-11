8c96dd257ccf9591cc536c95e15bf7b1a72937e3
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-11 14:46:12 +0000

    Added a compilation projectile command

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\""))
+  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"example2.opb\"")
+  (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (lsp-file-watch-threshold)))
 
