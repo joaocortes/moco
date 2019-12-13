5b40ffe157de3c69f7a6c372e3006eeb2598b17c
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-13 18:13:48 +0000

    Starting the implementation of the test script

---
+++ finalResults/.dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (projectile-project-run-cmd . "cd ./finalResults/; python3 ./main.py"))
+ (python-mode
+  (elpy-shell-starting-directory . "/home/joaooneillcortes/OneDrive/Documents/escola/doutoramento/office/tools/moco/finalResults/")
+  (gud-pdb-command-name . "python3 -m pdb main.py")))
+
+
+
