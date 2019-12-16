a320c95de373e81f544e6e9ef7a2c3d7f9239b66
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-16 13:00:27 +0000

    Tweaking elpy

--- finalResults/.dir-locals.el
+++ finalResults/.dir-locals.el
@@ -4,7 +4,7 @@
 ((nil
   (projectile-project-run-cmd . "cd ./finalResults/; python3 ./main.py"))
  (python-mode
-  (elpy-shell-starting-directory . "/home/joaooneillcortes/OneDrive/Documents/escola/doutoramento/office/tools/moco/finalResults/")
+  (elpy-shell-starting-directory . "/home/joaooneillcortes/OneDrive/Documents/escola/doutoramento/office/tools/moco/finalResults/finalResults")
   (gud-pdb-command-name . "python3 -m pdb main.py")))
 
 
