6c0e3c6099f487b64da8a17afe1a1b1b490993aa
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-16 17:02:57 +0000

    Corrected the run command, and parametrized the test function

--- finalResults/.dir-locals.el
+++ finalResults/.dir-locals.el
@@ -2,10 +2,10 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (projectile-project-run-cmd . "cd ./finalResults/; python3 ./main.py"))
+  (projectile-project-run-cmd . "cd ./finalResults/; python3 ./test.py"))
  (python-mode
   (elpy-shell-starting-directory . "/home/joaooneillcortes/OneDrive/Documents/escola/doutoramento/office/tools/moco/finalResults/finalResults")
-  (gud-pdb-command-name . "python3 -m pdb main.py")))
+  (gud-pdb-command-name . "python3 -m pdb test.py")))
 
 
 
--- finalResults/finalResults/test.py
+++ finalResults/finalResults/test.py
@@ -1,7 +1,8 @@
 import subprocess
 import os
 
-javaJarName = "../target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+javaJarName = """../target/org.sat4j.moco.threeAlgorithms-
+0.0.1-SNAPSHOT-jar-with-dependencies.jar"""
 testsPath = "./convertedInstances/"
 outputPath = "./output/"
 watcherFilePrefix = "watcher_"
@@ -17,36 +18,31 @@ testsPath = absolutize(testsPath)
 outputPath = absolutize(outputPath)
 
 
-
-def test():
+def test(time: int):
     for fileName in os.listdir(testsPath):
         print("fileName:" + fileName)
         for solverI in range(3):
             # print(os.path.join(testsPath, fileName))
-            runSolver(fileName, solverI)
+            runSolver(fileName, solverI, time)
 
 
-def runSolver(fileName: str, solverI: int):
-    outputName = os.path.basename(fileName)+".out"
-    command = "runsolver  -W  20  --timestamp "
-    command += "-w " + os.path.join(outputPath, watcherFilePrefix + outputName) + " "
-    command += "-o " + os.path.join(outputPath, solverOutputFilePrefix + outputName) + " "
+def runSolver(fileName: str, solverI: int, time):
+    outputName = os.path.basename(fileName)
+    outputName = os.path.splitext(outputName)[0]
+    outputName += "_S"+str(solverI)+".out"
+    command = "runsolver  -W " + str(time) + " "
+    command += "  --timestamp "
+    command += "-w " + os.path.join(
+        outputPath, watcherFilePrefix + outputName) + " "
+    command += "-o " + os.path.join(
+        outputPath, solverOutputFilePrefix + outputName) + " "
     command += " java -jar " + javaJarName + " "
     command += os.path.join(testsPath, fileName) + " "
+    command += "-v 2 "
     command += "-alg " + str(solverI)
 
     # print(command)
     subprocess.call(command, shell=True)
 
 
-    # subprocess.call([
-    #     "runsolver", " -W", " 20", " --timestamp",
-    #     "-w " + watcherFile,
-    #     "-o " + solverOutputFile,
-    #     "java", "-jar " + javaJarName,
-    #     fileName,
-    #     "-alg", str(solverI)
-    # ])
-
-
-test()
+test(1)
