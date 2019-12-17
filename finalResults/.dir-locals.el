4d05518f394f55bc59f7d42017dc956e84a4d425
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-17 08:21:47 +0000

    Corrected the projectile run command

--- finalResults/.dir-locals.el
+++ finalResults/.dir-locals.el
@@ -2,10 +2,10 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (projectile-project-run-cmd . "cd ./finalResults/; python3 ./test.py"))
+  (projectile-project-run-cmd . "python3 ./finalResults/test.py"))
  (python-mode
   (elpy-shell-starting-directory . "/home/joaooneillcortes/OneDrive/Documents/escola/doutoramento/office/tools/moco/finalResults/finalResults")
-  (gud-pdb-command-name . "python3 -m pdb test.py")))
+  (gud-pdb-command-name . "python3 -m pdb ./finalResults/test.py")))
 
 
 
