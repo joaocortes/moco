cb199ad00934b02f05b7c60a315c6c7265c03f25
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-19 11:35:12 +0000

    The runout parameters can now be given as command line options

--- finalResults/finalResults/test.py
+++ finalResults/finalResults/test.py
@@ -6,9 +6,9 @@ import os
 
 class Tester():
 
-    def __init__(self, time, memoryKB: int):
-        self.time = time
-        self.memoryKB = memoryKB
+    def __init__(self):
+        self.time = None
+        self.memoryKB = None
         self.javaJarName = ("../target/org.sat4j.moco.threeAlgorithms-"
                             "0.0.1-SNAPSHOT-jar-with-dependencies.jar")
         self.testsPath = "./convertedInstances/"
@@ -16,9 +16,6 @@ class Tester():
         self.watcherFilePrefix = "watcher_"
         self.solverOutputFilePrefix = "solver_"
         self.runSolverPath = "./runsolver"
-        self.javaJarName = self.absolutize(self.javaJarName)
-        self.testsPath = self.absolutize(self.testsPath)
-        self.runSolverPath = self.absolutize(self.runSolverPath)
 
     def readArguments(self):
 
@@ -31,17 +28,19 @@ class Tester():
         parser.add_argument("-alg", type=int, dest="algorithm", default=3,
                             help="algorithm to run. 0-paretoMCS, 1-unsatSat,\
                             2-pMinimal, 3-all intercalated")
-
+        parser.add_argument("-t", type=int, dest="time", default=10,
+                            help="runout time")
+        parser.add_argument("-m", type=int, dest="memoryKB", default=9126000,
+                            help="runout memory, in KB")
         args = parser.parse_args(sys.argv[1:])
         print("args: %r\n" % args)
 
         if args.sandbox == "1":
             self.outputPath = os.path.join("./sandbox/", self.outputPath)
             if not os.path.exists(self.absolutize("./sandbox/")):
-                os.mkdir(self.absolutize("./sandbox/"))
-        self.outputPath = self.absolutize(self.outputPath)
+                os.makedirs(self.absolutize("./sandbox/output/"))
 
-        return args.algorithm
+        return args.algorithm, args.time, args.memoryKB
 
     def absolutize(self, relativePath):
         return os.path.abspath(relativePath)
@@ -53,7 +52,7 @@ class Tester():
             solverRange = range(3)
 
         for fileName in os.listdir(self.testsPath):
-            print("fileName:" + fileName)
+            print("fileName: " + fileName)
             for solverI in solverRange:
                 # print(os.path.join(testsPath, fileName))
                 self.runSolver(fileName, solverI)
@@ -62,6 +61,26 @@ class Tester():
         outputName = os.path.basename(fileName)
         outputName = os.path.splitext(outputName)[0]
         outputName += "_S"+str(solverI)+".out"
+
+        self.javaJarName = self.absolutize(self.javaJarName)
+        self.testsPath = self.absolutize(self.testsPath)
+        self.runSolverPath = self.absolutize(self.runSolverPath)
+        self.outputPath = self.absolutize(self.outputPath)
+
+        command = (self.runSolverPath + " "
+                   "-W " + str(self.time) + " "
+                   "-M " + str(self.memoryKB) + " "
+                   "--timestamp "
+                   "-w " + os.path.join(
+                       self.outputPath,
+                       self.watcherFilePrefix + outputName) + " "
+                   "-o " + os.path.join(
+                       self.outputPath,
+                       self.solverOutputFilePrefix + outputName) + " "
+                   "java -jar " + self.javaJarName + " "
+                   "" + os.path.join(self.testsPath, fileName) + " "
+                   #  "-v 2 "
+                   "-alg " + str(solverI))
 
         command = (self.runSolverPath + " "
                    "-W " + str(self.time) + " "
@@ -82,6 +101,8 @@ class Tester():
         subprocess.call(command, shell=True)
 
 
-tester = Tester(120, 9126000)
-algorithm = tester.readArguments()
+tester = Tester()
+[algorithm, time, memoryKB] = tester.readArguments()
+tester.memoryKB = memoryKB
+tester.time = time
 tester.test(algorithm)
