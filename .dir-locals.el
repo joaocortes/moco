2c0f411e37e4a1bfb1afaa42f0b75ffee58b838a
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-19 12:01:56 +0000

    dir local variable were broken

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,11 +1,6 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-;;;
-
 ((nil
   (projectile-project-run-cmd . "java -jar ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/example2.opb")
 			      ;;;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/example2.opb\"
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (lsp-file-watch-threshold)))
-
