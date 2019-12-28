b0bb14027e1e55d52104706888a0469489ea445b
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-28 19:22:30 +0000

    New dir local variable: dap templates

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
 ((nil
-  (projectile-project-run-cmd . "java -jar ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/example2.opb")
-			      ;;;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/example2.opb\"
+  (projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/example2.opb")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
+  (dap-debug-template-configurations .  (("Run Configuration" :type "java" :request "launch" :args "examples/example2.opb" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+  :classPaths nil :name "Run Configuration" :projectName nil :mainClass nil)))
   (lsp-file-watch-threshold)))
+
