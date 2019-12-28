e4b47123c2edffff6bc2d09b7ec17e38e40e8c25
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-28 19:22:50 +0000

    Reshuffling

---
+++ dir-locals.el
@@ -0,0 +1,2 @@
+  (dap-debug-template-configurations .  (("Run Configuration" :type "java" :request "launch" :args "examples/example2.opb" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+  :classPaths nil :name "Run Configuration" :projectName nil :mainClass nil)))
