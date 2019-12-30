06565212dd6df9a884d024ceecb8aab0cd80a88c
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2019-12-30 18:59:50 +0000

    Still no screening, but working all right

---
+++ finalResults/finalResults/shellInterface.py
@@ -0,0 +1,42 @@
+import socket
+import shlex
+
+servers = ("centaurus", "scutum", "octans")
+gateway = "aquila"
+superServer = servers[0]
+me = "JC"
+
+
+def escapeCommand(command: str):
+    tokens = shlex.split(command)
+    command = ' '.join([shlex.quote(token) for token in tokens])
+    return command
+
+
+def buildCommand(tokens):
+    command = ' '.join(tokens)
+    return command
+
+
+def screenifyCommand(command: str):
+    tokens = ["screen", "-dm", command]
+    command = buildCommand(tokens)
+    return command
+
+
+def sshServer(command: str, server: str, location: str, screenify=True):
+
+    tokens = ["cd", shlex.quote(location), ";", command]
+    commandToSend = buildCommand(tokens)
+    if screenify:
+        commandToSend = screenifyCommand(commandToSend)
+    tokens = []
+    hostname = socket.gethostname()
+    if hostname == superServer:
+        if not server == hostname:
+            tokens = ["ssh", server, "-F", "/dev/null", shlex.quote(commandToSend)]
+    else:
+        tokens1 = ["ssh ", server, "-F", "/dev/null ", shlex.quote(commandToSend)]
+        tokens = ["ssh", gateway, shlex.quote(buildCommand(tokens1))]
+    command = buildCommand(tokens)
+    return command
