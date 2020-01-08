8fe18a2d00834b39623f8bf4966736ca4bc0e65b
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-01-08 14:29:57 +0000

    routine

--- src/main/java/org/sat4j/moco/problem/GoalDelimeter.java
+++ src/main/java/org/sat4j/moco/problem/GoalDelimeter.java
@@ -5,11 +5,7 @@ import org.sat4j.specs.IVecInt;
 public interface GoalDelimeter{
 
     public void prettyPrintVecInt(IVecInt vecInt);
-    public int getY(int iObj, int kD);
     public void UpdateCurrentK(int iObj, int upperKD);
-    public int getIObjFromY(int id);
-    public int getKDFromY(int id);
-    public boolean isY(int literal);
     public void prettyPrintLiteral(int literal);
     
 }
