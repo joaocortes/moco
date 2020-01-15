3de7a279116522e3cb0535f9763399800c9f713d
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-01-15 11:52:10 +0000

    dap debug template to test example1 with the unsatSat algorithm

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,7 +5,7 @@
   (projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/example2.opb")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
-  (dap-debug-template-configurations .  (("Run Configuration" :type "java" :request "launch" :args "examples/example2.opb" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+  (dap-debug-template-configurations .  (("Run unsatSat on example 1" :type "java" :request "launch" :args "examples/example1.opb -alg 1" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
   :classPaths nil :name "Run Configuration" :projectName nil :mainClass nil)))
   (lsp-file-watch-threshold)))
 
