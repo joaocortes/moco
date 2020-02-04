2431d00b51b01d7b03d5a72c6af419eea828b5da
Joao Cortes <jcortes@centaurus.inesc-id.pt>
2020-02-04 18:31:47 +0000

    Not able to recreate run conditions..

--- finalResults/finalResultsSC/run2/shellInterface.py
+++ finalResults/finalResultsSC/run2/shellInterface.py
@@ -1,1 +1,38 @@
-../shellInterface.py
+import socket
+import shlex
+from localRun import *
+superServer = servers[0]
+
+def escapeCommand(command: str):
+    tokens = shlex.split(command)
+    command = ' '.join([shlex.quote(token) for token in tokens])
+    return command
+
+
+def buildCommand(tokens):
+    command = ' '.join(tokens)
+    return command
+
+
+def screenifyCommand(command: str):
+    # tokens = ["screen", "-dm", "\"", command, "\""]
+    # command = buildCommand(tokens)
+    return command
+
+
+def sshServer(command: str, server: str, location: str, screenify=True):
+
+    tokens = ["cd", shlex.quote(location), ";", command]
+    commandToSend = buildCommand(tokens)
+    # if screenify:
+    #     commandToSend = screenifyCommand(commandToSend)
+    tokens = []
+    hostname = socket.gethostname()
+    if hostname == superServer:
+        if not hostname == server:
+            tokens = ["ssh", server, "-F", "/dev/null", shlex.quote(commandToSend)]
+    else:
+        tokens1 = ["ssh ", server, "-F", "/dev/null ", shlex.quote(commandToSend)]
+        tokens = ["ssh", gateway, shlex.quote(buildCommand(tokens1))]
+    command = buildCommand(tokens)
+    return command
