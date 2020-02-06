8894d2cdc151ab156d9064c78c4e411a0a2f484d
Joao Cortes <jcortes@centaurus.inesc-id.pt>
2020-02-06 13:50:19 +0000

    The choice of algorithms to run should be done in the localRun file,
    instead of using the command line options

--- finalResults/finalResultsSC/run2/localRun.py
+++ finalResults/finalResultsSC/run2/localRun.py
@@ -4,6 +4,9 @@ testsPath = "instances/"
 servers = ("centaurus",)
 gateway = "aquila"
 location = "./moco/finalResults/finalResultsSC/run2/"
-tester = Tester.Tester(location, servers, gateway)
+algorithms = (1, 2)
+
+
+tester = Tester.Tester(location, servers, gateway, algorithms)
 tester.fillParameters()
 tester.test()
