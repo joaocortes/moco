0507f9487b8a8424952e7766d4e102b0817f113c
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-03-28 20:41:47 +0000

    new dap template, to debug upperLimit bug

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,7 +7,7 @@
   (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/example2.opb -alg 1\"")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
-  (dap-debug-template-configurations .  (("Run unsatSat on example2" :type "java" :request "launch" :args "examples/example2.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+  (dap-debug-template-configurations .  (("Run unsatSat on bugupperlimit" :type "java" :request "launch" :args "examples/bugUpperLimit.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "Run Configuration" ;; :projectName  "org.sat4j.moco"
  :mainClass  "org.sat4j.moco.Launcher")("Run unsatSat on 10192" :type "java" :request "launch" :args "finalResults/finalResultsSC/run1/instances/bp-100-20-3-10-10192-SC.pbmo -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "Run Configuration" ;; :projectName  "org.sat4j.moco"
