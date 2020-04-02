bdcf1d8aa35851c74062c430dedc5e62d30e8d1a
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-04-02 19:31:50 +0100

    Abstracted problem, result, solver

--- src/main/java/org/sat4j/moco/algorithm/algorithm.java
+++ src/main/java/org/sat4j/moco/algorithm/algorithm.java
@@ -1,9 +1,31 @@
 package org.sat4j.moco.algorithm;
 
 import org.sat4j.moco.analysis.Result;
+import org.sat4j.moco.pb.PBSolver;
+import org.sat4j.moco.problem.Instance;
+import org.sat4j.moco.util.Log;
 
-public interface algorithm{
-    public Result getResult();
-    public void printFlightRecord();
 
+abstract public class  algorithm{
+
+    /**
+     * An instance of a MOCO problem to be solved.
+     */
+    protected Instance problem = null;
+
+    /**
+     * Stores the result (e.g. nondominated solutions) of the execution of the Pareto-MCS algorithm.
+     */
+    protected Result result = null;
+
+    /**
+     * Stores the PB solver to be used by the Pareto-MCS algorithm.
+     */
+    protected PBSolver solver = null;
+
+    /**
+     * Retrieves the result of the last call to {@link #solve()}.
+     * @return The result.
+     */
+    public Result getResult() { return this.result; }
 }
