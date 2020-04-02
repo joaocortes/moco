4d71d6902b98190539f3cb09b68734545d6e36f0
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-04-02 19:50:44 +0100

    Abstracted addClause

--- src/main/java/org/sat4j/moco/algorithm/algorithm.java
+++ src/main/java/org/sat4j/moco/algorithm/algorithm.java
@@ -1,9 +1,11 @@
 package org.sat4j.moco.algorithm;
 
 import org.sat4j.moco.analysis.Result;
+import org.sat4j.moco.pb.PBFactory;
 import org.sat4j.moco.pb.PBSolver;
 import org.sat4j.moco.problem.Instance;
 import org.sat4j.moco.util.Log;
+import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.IVecInt;
 
 
@@ -42,6 +44,25 @@ abstract public class  algorithm{
     public Result getResult() { return this.result; }
 
     abstract public void prettyPrintVecInt(IVecInt vecInt, boolean clausing);
+
+    /**
+     *Adds the disjunction of setOfLiterals
+     *@param setOfliterals
+     */
+    public boolean AddClause(IVecInt setOfLiterals){
+	this.prettyPrintVecInt(setOfLiterals,true);
+	for(int i = 0; i < setOfLiterals.size(); ++i)
+	try{
+	    this.solver.addConstr(PBFactory.instance().mkClause(setOfLiterals));
+	} catch (ContradictionException e) {
+	    Log.comment(6, "contradiction when adding clause: ");
+	    for(int j = 0; j < setOfLiterals.size(); ++j)
+		Log.comment(3, " " + setOfLiterals.get(j) + " " );
+	    return false;
+	}
+	return true;
+    }
+
     public void printFlightRecord(){
 	Log.comment(1, "aux constraints: " + this.auxiliarConstraintsN);
 	Log.comment(1, "aux Variables: " + this.auxiliarVariablesN);
