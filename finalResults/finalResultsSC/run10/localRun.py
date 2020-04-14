4de43133ca3f9cef178425199b199a6f7d095ec4
Joao Cortes <jcortes@centaurus.inesc-id.pt>
2020-04-14 09:47:01 +0100

    run10 localRun.py ready

--- finalResults/finalResultsSC/run10/localRun.py
+++ finalResults/finalResultsSC/run10/localRun.py
@@ -3,8 +3,8 @@ import Tester
 testsPath = "instances/"
 # taurus spica serpens sextans pictor pegasus scutum musca octans
 # centaurus
-servers = ("taurus", "spica", "serpens", "pictor", "pegasus",
-           "musca", "sextans")
+servers = ("taurus", "spica", "serpens", "sextans", "pictor", "pegasus",
+           "scutum", "musca", "octans")
 gateway = "aquila"
 location = "./moco/finalResults/finalResultsSC/run10/"
 algorithms = (1,)
