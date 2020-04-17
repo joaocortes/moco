69f084833076fe97a4d010806b9b1d217027a21b
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-04-17 13:23:21 +0100

    dap template

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,7 +7,7 @@
   (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/example2.opb -alg 1\"")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
-  (dap-debug-template-configurations .  (("Run unsatSat on bug15454gte_2" :type "java" :request "launch" :args "bugs/bug15454GTE_2/minimal.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+  (dap-debug-template-configurations .  (("Run unsatSat on bugIAmFresh2" :type "java" :request "launch" :args "bugs/bugIAmFresh2/minimal.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "Run Configuration" ;; :projectName  "org.sat4j.moco"
  :mainClass  "org.sat4j.moco.Launcher")("Run unsatSat on bugupperlimit" :type "java" :request "launch" :args "examples/bugUpperLimit.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "Run Configuration" ;; :projectName  "org.sat4j.moco"
