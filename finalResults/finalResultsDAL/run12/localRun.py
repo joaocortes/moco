9a3e27099558c8d457fd110abd94c5a0cabb8e58
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-04-21 13:02:35 +0100

    run12 genesis. Everything ready

---
+++ finalResults/finalResultsDAL/run12/localRun.py
@@ -0,0 +1,18 @@
+# run9 = runDAL1
+import Tester
+
+testsPath = "instances/"
+# taurus spica serpens sextans pictor pegasus scutum musca octans
+# centaurus
+servers = ("taurus", "spica", "sextans", "pictor", "pegasus",)
+gateway = "aquila"
+location = "./moco/finalResults/finalResultsDAL/run1"
+algorithms = (1,)
+encoder = "GTE"
+time = 3600
+useSandbox = 0
+
+tester = Tester.Tester(location, servers, gateway, algorithms,
+                       encoder, time, useSandbox, 3)
+# tester.fillParameters()
+tester.test()
