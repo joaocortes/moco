b89c78de271b92f4ce65665ac60633dc9e1aeaab
Joao Cortes <jcortes@centaurus.inesc-id.pt>
2020-04-23 00:12:51 +0100

    run17 genesis: GTE, unsatSat part1 Flying Turist Problem

---
+++ finalResults/finalResultsFTP/run/localRun.py
@@ -0,0 +1,18 @@
+# run15
+import Tester
+
+testsPath = "instances/"
+# taurus spica serpens sextans pictor pegasus scutum musca octans
+# centaurus
+servers = ("taurus", "spica", "sextans", "pictor", "pegasus",)
+gateway = "aquila"
+location = "./moco/finalResults/finalResultsFTP/run16"
+algorithms = (1,)
+encoder = "GTE"
+time = 3600
+useSandbox = 0
+
+tester = Tester.Tester(location, servers, gateway, algorithms,
+                       encoder, time, useSandbox, 3)
+# tester.fillParameters()
+tester.test()
