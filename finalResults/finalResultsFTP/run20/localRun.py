0e61878508d6734ac0fd180d8b7e68bd8eeba5c6
Joao Cortes <jcortes@centaurus.inesc-id.pt>
2020-04-24 01:05:20 +0100

    run20 genesis, GTE FTP unsatSat

--- finalResults/finalResultsFTP/run20/localRun.py
+++ finalResults/finalResultsFTP/run20/localRun.py
@@ -1,18 +1,18 @@
-# run17
+# run20
 import Tester
 
 testsPath = "instances/"
 # taurus spica serpens sextans pictor pegasus scutum musca octans
 # centaurus
-servers = ("taurus", "pegasus",)
+servers = ("spica", "serpens", "sextans",)
 gateway = "aquila"
-location = "./moco/finalResults/finalResultsFTP/run17"
+location = "./moco/finalResults/finalResultsFTP/run20"
 algorithms = (1,)
 encoder = "GTE"
 time = 3600
 useSandbox = 0
 
 tester = Tester.Tester(location, servers, gateway, algorithms,
-                       encoder, time, useSandbox, 3)
+                       encoder, time, useSandbox, 4)
 # tester.fillParameters()
 tester.test()
