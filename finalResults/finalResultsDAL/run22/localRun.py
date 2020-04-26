21977a057174ea033b6440555aaa2b7dcb924d90
Joao Cortes <jcortes@centaurus.inesc-id.pt>
2020-04-26 23:08:36 +0100

    servers choice

--- finalResults/finalResultsDAL/run22/localRun.py
+++ finalResults/finalResultsDAL/run22/localRun.py
@@ -4,7 +4,7 @@ import Tester
 testsPath = "instances/"
 # taurus spica serpens sextans pictor pegasus scutum musca octans
 # centaurus
-servers = ("taurus", "spica", "serpens", "pictor", "pegasus",)
+servers = ("sextans", "pictor",)
 gateway = "aquila"
 location = "./moco/finalResults/finalResultsDAL/run22"
 algorithms = (0,)
@@ -13,6 +13,6 @@ time = 3600
 useSandbox = 0
 
 tester = Tester.Tester(location, servers, gateway, algorithms,
-                       encoder, time, useSandbox, 2)
+                       encoder, time, useSandbox, 4)
 # tester.fillParameters()
 tester.test()
