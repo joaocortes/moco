d55a6afb52b7b008eee9f6aad2a1e63a20beeca0
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-05-21 21:00:50 +0100

    better projectile default commands

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,9 +2,9 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  ;;;(projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/example2.opb")
+  ;;;(projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/exampleX.opb")
   (lsp-file-watch-threshold)
-  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/example2.opb -alg 1\"")
+  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleX.opb -alg 1\"")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (dap-debug-template-configurations .  (
