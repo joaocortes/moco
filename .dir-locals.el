6fe75137faf2ac2cc4664ad3a955260624e2d2bd
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-05-24 12:07:24 +0100

    projectile run command uses MSU3 algorithm

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,11 +4,11 @@
 ((nil
   ;;;(projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/exampleX.opb")
   (lsp-file-watch-threshold)
-  (projectile-project-run-cmd . "mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleX.opb -alg 1\"")
+  (projectile-project-run-cmd . "cat examples/exampleX.opb;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleX.opb -alg 3\"")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (dap-debug-template-configurations .  (
-("Run unsatSat on exampleX" :type "java" :request "launch" :args "examples/exampleX.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+("Run unsatSat on exampleX" :type "java" :request "launch" :args "examples/exampleX.opb -alg 3 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "Run Configuration" ;; :projectName  "org.sat4j.moco"
  :mainClass  "org.sat4j.moco.Launcher")
 ("Run unsatSat on bugIAmFresh2" :type "java" :request "launch" :args "bugs/bugIAmFresh2/minimal.opb -alg 1 -v 6" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
