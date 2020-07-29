71e82efb528af27ac27dbd677f148902edb6015e
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-07-29 22:51:37 +0100

    corrected SelectionDelimeter imports

--- src/main/java/org/sat4j/moco/problem/SelectionDelimeter.java
+++ src/main/java/org/sat4j/moco/problem/SelectionDelimeter.java
@@ -37,7 +37,6 @@ import org.sat4j.core.ReadOnlyVec;
 import org.sat4j.core.VecInt;
 import org.sat4j.moco.util.Real;
 import org.sat4j.moco.pb.PBSolver;
-import org.sat4j.moco.problem.SelectionDelimeter.Circuit.SelectionComponent;
 import org.sat4j.specs.IVecInt;
 
 
