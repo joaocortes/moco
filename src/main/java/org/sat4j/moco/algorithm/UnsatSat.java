97d775c13c8bc9207c5f602bd2add90f358b1197
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-08-05 12:03:23 +0100

    UnsatSat: using goalDelimeter.generateUpperBoundAssumptions

--- src/main/java/org/sat4j/moco/algorithm/UnsatSat.java
+++ src/main/java/org/sat4j/moco/algorithm/UnsatSat.java
@@ -218,7 +218,7 @@ public class UnsatSat extends algorithm {
 		    this.logExhaustedUpperKD();
 		    this.updateUpperBound(currentExplanation);
 		    this.preAssumptionsExtend();
-		    currentAssumptions = this.generateUpperBoundAssumptions();
+		    currentAssumptions = this.goalDelimeter.generateUpperBoundAssumptions(this.UpperKD);
 
 		    }}
 	    }
