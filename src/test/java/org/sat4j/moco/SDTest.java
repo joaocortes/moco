20a3d8793bfe0015e610ae8e1d06695a0a755d5e
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-08-06 19:54:40 +0100

    big chunck

---
+++ src/test/java/org/sat4j/moco/SDTest.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * SAT4J: a SATisfiability library for Java Copyright (C) 2004, 2012 Artois University and CNRS
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU Lesser General Public License Version 2.1 or later (the
+ * "LGPL"), in which case the provisions of the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of the LGPL, and not to allow others to use your version of
+ * this file under the terms of the EPL, indicate your decision by deleting
+ * the provisions above and replace them with the notice and other provisions
+ * required by the LGPL. If you do not delete the provisions above, a recipient
+ * may use your version of this file under the terms of the EPL or the LGPL.
+ *
+ * Contributors:
+ *   CRIL - initial API and implementation
+ *   Miguel Terra-Neves, Ines Lynce and Vasco Manquinho - MOCO solver
+ *******************************************************************************/
+package org.sat4j.moco;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.sat4j.moco.problem.SelectionDelimeter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.sat4j.core.Vec;
+import org.sat4j.core.VecInt;
+import org.sat4j.moco.algorithm.algorithm;
+
+import org.sat4j.moco.analysis.Result;
+import org.sat4j.moco.pb.PBFactory;
+import org.sat4j.moco.problem.Instance;
+import org.sat4j.moco.problem.Objective;
+import org.sat4j.moco.util.Real;
+import org.sat4j.moco.problem.LinearObj;
+
+public class SDTest {
+    protected SelectionDelimeter sd = null;
+    protected Instance moco;
+    protected LinearObj main_obj;
+    protected chosenAlgorithm solver;
+    protected abstract chosenAlgorithm instateAlgorithm();
+
+    public SDTest(){};
+    @Before
+    public void partialSetUp() {
+	    this.moco = new Instance();
+	    this.sd = null;
+	    this.moco.addConstr(PBFactory.instance().mkGE(new VecInt(new int[] { 1, 2, 3 }), 2));
+	    this.main_obj = new LinearObj(new VecInt(new int[] { 1, 2 }),
+                                      new Vec<Real>(new Real[] { new Real(2), Real.ONE }));
+	    this.moco.addObj(this.main_obj);
+    }
+}
