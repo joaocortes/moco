e269d40e0f66cb42d64b9e558be09f0c24ce00ee
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-09-23 22:08:25 +0100

    exampleXX.pbmo is the default debugging instance

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,11 +4,11 @@
 ((nil
   ;;;(projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/exampleX.opb")
   (lsp-file-watch-threshold)
-  (projectile-project-run-cmd . "export MAVEN_OPTS=\"-ea\";cat examples/exampleX.opb;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleX.opb -alg 1 -enc \\\"SD\\\"\"")
+  (projectile-project-run-cmd . "export MAVEN_OPTS=\"-ea\";cat examples/exampleXX.opb;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleXX.opb -alg 1 -enc \\\"SD\\\"\"")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (dap-debug-template-configurations .  (
-("Run unsatSat on exampleX" :type "java" :request "launch" :args "examples/exampleX.opb -alg 1 -v 6 -enc \"GTE\"" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+("Run unsatSat on exampleXX" :type "java" :request "launch" :args "examples/exampleXX.opb -alg 3 -v 6 -enc \"GTE\"" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "Run Configuration" ;; :projectName  "org.sat4j.moco"
  :mainClass  "org.sat4j.moco.Launcher")
 ("Run unsatSat on bugNonMonotonous" :type "java" :request "launch" :args "bugs/bugNonMonotonous/minimal.pbmo -alg 1 -v 6 -enc \"SD\"" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
