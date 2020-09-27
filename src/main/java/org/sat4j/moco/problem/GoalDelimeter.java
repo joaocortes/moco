126264a9405ac54f63423c9bcd912cf3e2a142cf
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-09-27 01:14:20 +0100

    do not log all clauses added by the GoalDelimeter

--- src/main/java/org/sat4j/moco/problem/GoalDelimeter.java
+++ src/main/java/org/sat4j/moco/problem/GoalDelimeter.java
@@ -178,7 +178,7 @@ public abstract class GoalDelimeter<PIndex extends GoalDelimeter.Index>{
      */
 
     public boolean AddClause(IVecInt setOfLiterals){
-	this.prettyPrintVecInt(setOfLiterals);
+	// this.prettyPrintVecInt(setOfLiterals);
 	try{
 	    this.solver.AddClause(setOfLiterals);
 	} catch (ContradictionException e) {
