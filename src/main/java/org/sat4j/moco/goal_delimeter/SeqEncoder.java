28fde5eb46fdb41b7b33af560fa7e35095a8220d
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-10-12 17:47:48 +0100

    innocuous

--- src/main/java/org/sat4j/moco/goal_delimeter/SeqEncoder.java
+++ src/main/java/org/sat4j/moco/goal_delimeter/SeqEncoder.java
@@ -34,6 +34,7 @@ import org.sat4j.core.VecInt;
 import org.sat4j.moco.util.Real;
 import org.sat4j.moco.pb.PBSolver;
 import org.sat4j.moco.problem.Instance;
+import org.sat4j.moco.problem.Objective;
 import org.sat4j.specs.IVecInt;
 import org.sat4j.specs.ContradictionException;
 
