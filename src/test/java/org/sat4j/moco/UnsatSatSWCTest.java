ebb3ab2a26ea0f7d2fea75715d10a6528a9c8b55
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-10-18 17:43:56 +0100

    test suit now uses the new Creator mechanisms

--- src/test/java/org/sat4j/moco/UnsatSatSWCTest.java
+++ src/test/java/org/sat4j/moco/UnsatSatSWCTest.java
@@ -1,10 +1,10 @@
 package  org.sat4j.moco;
 import org.sat4j.moco.algorithm.UnsatSat;
+import org.sat4j.moco.algorithm.algorithm;
 
-public class UnsatSatSWCTest extends algorithmTest<UnsatSat> {
+public class UnsatSatSWCTest extends algorithmTest {
     public UnsatSatSWCTest(){};
-    public UnsatSat instateAlgorithm(){
-	return new UnsatSat(this.moco, "SWC");
-
+    public algorithm instateAlgorithm(){
+	return this.algCreator.create(1, "SWC", this.moco);
     }
 }
