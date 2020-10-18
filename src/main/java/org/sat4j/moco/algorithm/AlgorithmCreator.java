f276d7e269e24d1f93432d4111b4f7d710fff303
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-10-18 16:24:00 +0100

    algorithmCreator implemented and used

--- src/main/java/org/sat4j/moco/algorithm/AlgorithmCreator.java
+++ src/main/java/org/sat4j/moco/algorithm/AlgorithmCreator.java
@@ -1,7 +1,6 @@
 package org.sat4j.moco.algorithm;
 
 import org.sat4j.moco.Params;
-import org.sat4j.moco.goal_delimeter.GoalDelimeter;
 import org.sat4j.moco.goal_delimeter.GoalDelimeterCreator;
 import org.sat4j.moco.goal_delimeter.GoalDelimeterI;
 import org.sat4j.moco.pb.PBSolver;
@@ -12,32 +11,42 @@ import org.sat4j.moco.util.Log;
 public class AlgorithmCreator {
 
     public algorithm create(Params params, Instance moco){
+	String encoding = params.getEncodingGD();
+	int algorithmI =  params.getAlgorithmI();
+	return create(algorithmI, encoding, moco);
+    }
+    
+     public algorithm create(int AlgorithmI, String encoding,  Instance moco){
 	algorithm algorithm1 = null;
-	boolean MSU3 = false;
-	boolean withGoalDelimeter;
-	switch(params.getAlgorithmI()) {
+	switch(AlgorithmI) {
 	case 0:
 	    algorithm1 = new ParetoMCS(moco);
+	    return algorithm1;
 	case 1:
 	    UnsatSat unsatSat = new UnsatSat(moco);
-	    unsatSat.setGoalDelimeter(createGoalDelimeter(params, unsatSat, unsatSat.GetGoalDelimeter(), false));
+	    unsatSat.setGoalDelimeter(createGoalDelimeter(encoding, unsatSat, unsatSat.GetGoalDelimeter(), false));
+	    return unsatSat;
 	case 2:
-	    algorithm1 = new pMinimal(moco);
+	    pMinimal pmin = new pMinimal(moco);
+	    pmin.setGoalDelimeter(createGoalDelimeter(encoding, pmin, pmin.GetGoalDelimeter(), false));
+	    return pmin;
 	case 3:
 	    UnsatSatMSU3 unsatSatMSU3 = new UnsatSatMSU3(moco);
-	    unsatSatMSU3.setGoalDelimeter(createGoalDelimeter(params, unsatSatMSU3, unsatSatMSU3.GetGoalDelimeter(), true));	    
-	    break;
+	    unsatSatMSU3.setGoalDelimeter(createGoalDelimeter(encoding, unsatSatMSU3, unsatSatMSU3.GetGoalDelimeter(), true));	    
+	    return unsatSatMSU3;
 	default:
 	    Log.comment("Don't know what solver to use");
 	    break;
 	}
 	return algorithm1;
     }
-    
-    private  GoalDelimeterI createGoalDelimeter(Params params, algorithm algorithm1, GoalDelimeterI gd, boolean MSU3){
+
+
+
+    private  GoalDelimeterI createGoalDelimeter(String encoding, algorithm algorithm1, GoalDelimeterI gd, boolean MSU3){
 	PBSolver solver = algorithm1.getSolver();
 	Instance instance = algorithm1.getProblem();
-	gd = GoalDelimeterCreator.create(params.getEncodingGD(), instance , solver, MSU3);
+	gd = GoalDelimeterCreator.create(encoding, instance , solver, MSU3);
 	return gd;
     }
 
