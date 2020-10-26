958f931f83f462f312df6c4d5fa6cb18fd045aa8
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-10-26 17:23:51 +0000

    MSU3 abilities copied into SelectionDelimeterMSU3

--- src/main/java/org/sat4j/moco/goal_delimeter/SelectionDelimeterMSU3.java
+++ src/main/java/org/sat4j/moco/goal_delimeter/SelectionDelimeterMSU3.java
@@ -32,11 +32,14 @@ import java.util.Map.Entry;
 import java.util.HashMap;
 
 
-
+import org.sat4j.core.ReadOnlyVec;
+import org.sat4j.core.ReadOnlyVecInt;
 import org.sat4j.core.VecInt;
 import org.sat4j.moco.util.Log;
+import org.sat4j.moco.util.Real;
 import org.sat4j.moco.pb.PBSolver;
 import org.sat4j.moco.problem.Instance;
+import org.sat4j.moco.problem.Objective;
 import org.sat4j.moco.problem.DigitalEnv;
 import org.sat4j.moco.problem.DigitalEnv.DigitalNumber;
 import org.sat4j.specs.IVecInt;
@@ -59,13 +62,31 @@ public class SelectionDelimeterMSU3 extends SelectionDelimeterT<SelectionDelimet
 
     public SelectionDelimeterMSU3(Instance instance, PBSolver solver, boolean buildCircuit) {
 	super(instance, solver, buildCircuit);
+	this.generateY();
 	this.uncoveredMaxKD = new int[this.instance.nObjs()];
-
 	this.UpperBound =  new int[(this.instance.nObjs())];
+	this.coveredLiterals = new HashMap<Integer, Boolean>(this.solver.nVars());
+	this.initializeCoveredLiterals();
     }
 
 
+    private void initializeCoveredLiterals(){
+	for(int iObj = 0, nObj = this.instance.nObjs();iObj < nObj; iObj++){
+	    Objective ithObjective = this.instance.getObj(iObj);
+	    ReadOnlyVecInt objectiveLits = ithObjective.getSubObjLits(0);
+	    ReadOnlyVec<Real> objectiveCoeffs = ithObjective.getSubObjCoeffs(0);
+	    int sign = 1;
+	    int ithAbsoluteWeight;
+	    for(int iX = 0, nX = ithObjective.getTotalLits(); iX <nX; iX ++){
+		int ithX = objectiveLits.get(iX);
+		ithAbsoluteWeight = objectiveCoeffs.get(iX).asInt();
+		sign = (ithAbsoluteWeight > 0? 1 : -1);
+		ithAbsoluteWeight *= sign;
+		this.coveredLiterals.putIfAbsent(-sign * ithX, true);
+	    }
+	}
 
+    }
     // static class SDIndex extends Index{
 
     // 	SDIndex(int iObj, int kD){
@@ -230,7 +251,7 @@ public class SelectionDelimeterMSU3 extends SelectionDelimeterT<SelectionDelimet
 		    public int getFreshVar1(){return getSolver().getFreshVar();}
 		    public boolean AddClause1(IVecInt setOfLiterals){return AddClause(setOfLiterals);}
 		};
-
+	    this.circuit.buildCircuit();
 	}
 
 
@@ -292,6 +313,108 @@ public class SelectionDelimeterMSU3 extends SelectionDelimeterT<SelectionDelimet
 	public void setCoveredLiterals(HashMap<Integer, Boolean> coveredLiterals) {
 		this.coveredLiterals = coveredLiterals;
 	}
+
+    /**
+     *If necessary for the construction of the current assumptions,
+     *initialize more of the domain of the goal delimeter
+     */
+    @Override
+    public boolean preAssumptionsExtend(IVecInt currentExplanation){
+	Log.comment(2, "explanation: ");
+	this.prettyPrintVecInt(currentExplanation);
+	boolean change = false;
+	// Log.comment(0, "covered x variables: " + this.coveredLiterals.size());
+	IVecInt currentExplanationX = new VecInt(new int[] {});
+	HashMap<Integer,Boolean> objectivesToChange = new HashMap<Integer, Boolean>(this.instance.nObjs());
+	for(int lit: currentExplanation.toArray()){
+	    int id = this.solver.idFromLiteral(lit);
+	    if(this.isX(id)){
+		currentExplanationX.push(lit);
+		for(int iObj = 0; iObj < this.instance.nObjs(); ++iObj){
+		    if(this.instance.getObj(iObj).getSubObj(0).weightFromLit(id) != null)
+			objectivesToChange.put(iObj, null);
+		}
+	    }
+	    else
+		objectivesToChange.put(this.getIObjFromY(id), null);
+
+	}
+	change = this.uncoverXs(currentExplanationX);
+	for(int iObj :objectivesToChange.keySet()){
+	    // Log.comment(3, "changing upperlimit " + iObj);
+	    int upperKDBefore = this.getUpperKD(iObj);
+ 	    if(this.getUpperKD(iObj) == this.getUpperBound(iObj))
+		this.generateNext(iObj,this.getUpperKD(iObj), this.getMaxUncoveredKD(iObj));
+	    this.setUpperKD(iObj, this.nextKDValue(iObj, this.getUpperKD(iObj)));
+	    if(this.getUpperKD(iObj) >= this.getUpperBound(iObj))
+		this.generateNext(iObj, this.getUpperKD(iObj), this.getMaxUncoveredKD(iObj));
+	    this.setUpperBound(iObj, this.nextKDValue(iObj, this.getUpperKD(iObj)));
+	    if(this.getUpperKD(iObj)!= upperKDBefore)
+		change = true;
+	}
+	return change;
+
+    }
+	@Override
+	public IVecInt generateUpperBoundAssumptions(IVecInt explanation, boolean checkChange) {
+	    IVecInt assumptions =  super.generateUpperBoundAssumptions(explanation, checkChange);
+	    for(Integer x: this.coveredLiterals.keySet())
+		assumptions.push(x);
+	    
+	    Log.comment(2, "assumptions:");
+	    this.prettyPrintVecInt(assumptions);
+	    return assumptions;
+	}
+    /**
+     *Uncover leafs
+     */
+    private boolean uncoverXs(IVecInt explanationX) {
+	// Log.comment(5, "{ UnsatSatMSU3.uncoverXs");
+
+	int lit = 0;
+	for(int iLit = 0, n = explanationX.size(); iLit < n; iLit++){
+	    lit = explanationX.get(iLit);
+	    this.coveredLiterals.remove(lit);
+	}
+	this.updateAllUncoveredMaxKD();
+	this.logUncoveredMaxKD();
+	// Log.comment(5, "}");
+	return true;
+    }
+
+private void updateAllUncoveredMaxKD(){
+    for(int i = 0, n = this.instance.nObjs(); i < n; i++)
+	this.updateUncoveredMaxKD(i);
+}
+    public void logUncoveredMaxKD(){
+	String logUpperLimit = "uncovered max: ["+this.getUncoveredMaxKD(0);
+	for(int iObj = 1; iObj < this.instance.nObjs(); ++iObj)
+	    logUpperLimit +=", "+this.getUncoveredMaxKD(iObj) ;//+ this.instance.getObj(iObj).getMinValue())
+	//..log
+	
+	logUpperLimit +="]";
+	Log.comment(2, logUpperLimit );
+    }
+
+	public void updateUncoveredMaxKD(int iObj){
+	    int a = 0;
+	    Objective ithObjective = instance.getObj(iObj); // 
+	    ReadOnlyVecInt objectiveLits = ithObjective.getSubObjLits(0);
+	    ReadOnlyVec<Real> objectiveCoeffs = ithObjective.getSubObjCoeffs(0);
+	    int sign = 1;
+	    int ithAbsoluteWeight;
+	    for(int iX = 0, nX = ithObjective.getTotalLits(); iX < nX; iX ++){
+		int ithX = objectiveLits.get(iX);
+		ithAbsoluteWeight = objectiveCoeffs.get(iX).asInt();
+		sign = (ithAbsoluteWeight > 0? 1 : -1);
+		ithAbsoluteWeight *= sign;
+		if(coveredLiterals.get(-sign * ithX) == null)
+		    a += ithAbsoluteWeight;
+	    }
+	    this.setMaxUncoveredKD(iObj, a);
+	}
+    public void setMaxUncoveredKDs(int iObj, int a){this.uncoveredMaxKD[iObj] = a;}
+
     private int getUpperBound(int iObj){
 	return this.UpperBound[iObj];
     }
