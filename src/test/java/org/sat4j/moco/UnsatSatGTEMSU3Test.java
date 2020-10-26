cc423c3f0cadefb9a47dbe25f2a4f331797d2076
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-10-26 17:29:43 +0000

    UnsatSatSDMSU3 and UnsatSatGTEMSU3 tests

--- src/test/java/org/sat4j/moco/UnsatSatGTEMSU3Test.java
+++ src/test/java/org/sat4j/moco/UnsatSatGTEMSU3Test.java
@@ -2,10 +2,10 @@ package org.sat4j.moco;
 import org.sat4j.moco.algorithm.UnsatSatMSU3;
 import org.sat4j.moco.algorithm.algorithm;
 
-public class UnsatSatMSU3Test extends algorithmTest {
-    public UnsatSatMSU3Test(){};
+public class UnsatSatGTEMSU3Test extends algorithmTest {
+    public UnsatSatGTEMSU3Test(){};
     public algorithm instateAlgorithm(){
-	return this.algCreator.create(3, "SD", this.moco);
+	return this.algCreator.create(3, "GTE", this.moco);
     }
 
 
