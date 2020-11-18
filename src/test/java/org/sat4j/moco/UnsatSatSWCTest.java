756447bce6a6b4bc654f349bd7d1b0c0b2bee0c8
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-11-18 00:31:12 +0000

    Test disabled: UnsatSatSWCTest disabled

--- src/test/java/org/sat4j/moco/UnsatSatSWCTest.java
+++ src/test/java/org/sat4j/moco/UnsatSatSWCTest.java
@@ -1,7 +1,9 @@
 package  org.sat4j.moco;
+import org.junit.Ignore;
 import org.sat4j.moco.algorithm.UnsatSat;
 import org.sat4j.moco.algorithm.algorithm;
 
+@Ignore
 public class UnsatSatSWCTest extends algorithmTest {
     public UnsatSatSWCTest(){};
     public algorithm instateAlgorithm(){
