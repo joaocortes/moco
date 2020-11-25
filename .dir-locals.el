008b622e809a1dd92e3a35913a9c7bd06fd35132
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2020-11-25 22:15:41 +0000

    meta, commented out projectil-project-run-cmd alternative

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,7 @@
   ;;;(projectile-project-run-cmd . "java -jar  ./target/org.sat4j.moco.threeAlgorithms-0.0.1-SNAPSHOT-jar-with-dependencies.jar examples/exampleX.opb")
   (lsp-file-watch-threshold)
   (projectile-project-run-cmd . "export MAVEN_OPTS=\"-ea\";cat examples/exampleXX.opb;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleXX.opb -alg 1 -enc \\\"SD\\\"\"")
+  ;; (projectile-project-run-cmd . "export MAVEN_OPTS=\"-ea\";cat examples/exampleXX.opb;mvn exec:java -Dexec.mainClass=org.sat4j.moco.Launcher -Dexec.args=\"examples/exampleXX.opb -alg 1 -enc \\\"SD\\\"  -rb 0:[20,3,3] -ul 0:5\"")
   (projectile-project-compilation-cmd . "mvn -DskipTests=true package"))
  (java-mode
   (dap-debug-template-configurations .  (
