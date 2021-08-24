3224ef0ba9eeaf48ffb48d0812af5a1d5f36a6f2
JoaoOneillCortes <joaooneillcortes@outlook.pt>
2021-08-24 12:21:53 +0100

    debug template

--- .dir-locals.el
+++ .dir-locals.el
@@ -12,4 +12,7 @@
 ("Run unsatSat on exampleXX" :type "java" :request "launch" :args "examples/exampleXX.opb -alg 2 -v 6 -enc \"SD\"" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
  :name "example2" ;; :projectName  "org.sat4j.moco"
  :mainClass  "org.sat4j.moco.Launcher")
+("Run analyzer on DAL result" :type "java" :request "launch" :args "-v 4 /home/Superficial/Mnemosyne/Aion/moco/mocoData/runnerData/instances/DAL/sampleA/f1-DataDisplay_0_order4.seq-A-2-1-EDCBAir.pbmo 0,*:./bugs/bugVolume0/dal0 1,GTE:./bugs/bugVolume0/dal1 1,SD:./bugs/bugVolume0/dal2 2,SDtruncJ:./bugs/bugVolume0/dal3 2,SWC:./bugs/bugVolume0/dal4 3,GTE:./bugs/bugVolume0/dal5 3,GTE++:./bugs/bugVolume0/dal6 3,GTE+++:./bugs/bugVolume0/dal7 3,GTEGTE:./bugs/bugVolume0/dal8 3,SD:./bugs/bugVolume0/dal9 3,SDtrunc:./bugs/bugVolume0/dal10 4,SDTruncOU:./bugs/bugVolume0/dal11 4,SDtruncO:./bugs/bugVolume0/dal12 4,SDtruncULO:./bugs/bugVolume0/dal13" :cwd nil :stopOnEntry :json-false :host "localhost" :request "launch"
+ :name "analyze0" 
+ :mainClass  " org.sat4j.moco.analysis.Analyzer")
 ))))
